a2862c1cee4c512f365f438836044adc75da6018
Vladislav Hilko <uladzislau.hilko@gmail.com>
2024-08-30 01:01:01 -0700

    [Seeds Data] Add seeds data to the application

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,40 @@
-# frozen_string_literal: true
+puts "Seeding 20 surveys with random responses..."
 
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+questions = [
+  'Do you like working remotely?',
+  'Is coffee your preferred morning drink?',
+  'Do you enjoy reading books?',
+  'Is exercising part of your daily routine?',
+  'Do you prefer winter over summer?',
+  'Is pizza your favorite food?',
+  'Do you believe in life on other planets?',
+  'Is coding your passion?',
+  'Do you like traveling?',
+  'Is early morning the best time to work?',
+  'Do you play video games?',
+  'Is learning new languages fun?',
+  'Do you prefer cats over dogs?',
+  'Is working in silence better for you?',
+  'Do you enjoy watching movies?',
+  'Is spending time in nature relaxing?',
+  'Do you think technology improves life?',
+  'Is your work-life balance good?',
+  'Do you use social media daily?',
+  'Is working out important to you?'
+]
+
+questions.each do |question|
+  survey = CreateSurvey::EntryPoint.call(params: { question: question })
+
+  num_responses = rand(3..100)
+
+  num_responses.times do
+    answer = [true, false].sample
+
+    CreateSurveyResponse::EntryPoint.call(survey_id: survey.id, params: { answer: answer })
+  end
+
+  puts "Created survey with question: '#{question}' and #{num_responses} responses."
+end
+
+puts "Seeding completed successfully!"
